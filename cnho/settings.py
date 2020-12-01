4b822399fbbe424739a0d47ad008ddf0b1e2a002
nezaicev <nezaicev@yandex.ru>
2020-12-01 03:21:08 +0300

    Fixed problem live server

--- cnho/settings.py
+++ cnho/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
-        'USER': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT',''),
