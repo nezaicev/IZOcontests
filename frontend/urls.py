eeb868f4b9ba75870910617b7b244ff56772eefe
nezaicev <nezaicev@yandex.ru>
2022-04-14 13:34:20 +0300

    fix bug url

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('vp/', views.index),
-    path('api/archive/', views.ArchiveAPIView.as_view({'get': 'list'})),
+    path('api/archive', views.ArchiveAPIView.as_view({'get': 'list'})),
     path('api/archive/nominationvp', views.NominationVP_API_View.as_view())
 ]
