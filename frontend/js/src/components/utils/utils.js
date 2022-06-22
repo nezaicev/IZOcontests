20dd05bb381edf0f253ede6217cec0b35d0c0b4c
nezaicev <nezaicev@yandex.ru>
2022-06-22 10:49:24 +0300

    add check valid participants name

--- frontend/js/src/components/utils/utils.js
+++ frontend/js/src/components/utils/utils.js
@@ -1,27 +1,45 @@
-export function formattingName(name) {
 
+
+export function validContestName(name){
+    if ((name.search(/\d/) !== -1)||
+        ( name.toLowerCase().indexOf('изосту')!==-1 )||
+        ( name.toLowerCase().indexOf('колл')!==-1 )||
+        ( name.toLowerCase().indexOf('клас')!==-1 )||
+        ( name.toLowerCase().indexOf('учен')!==-1 )
+    ){
+
+        return false
+    }
+    else {
+        return true
+    }
+
+}
+
+
+
+
+
+export function formattingName(name) {
     if (name) {
         let data = name.split(', ')
         let result = ''
         data.forEach(function (item, i) {
             result += item.split(' ')[0][0] + '. ' + item.split(' ')[1] + ', '
-
         })
         return result.slice(0, -2)
     } else {
         return ''
     }
-
 }
 
-export  function createLabel(obj) {
+export function createLabel(obj) {
     let authorName = obj.author_name ? `«${obj.author_name.replace(/['"]+/g, '')}», ` : ''
-    let material=obj.material? `${obj.material}, ` : ''
+    let material = obj.material ? `${obj.material}, ` : ''
     let age = obj.age ? `${obj.age}, ` : ''
     let fio = obj.fio ? `${formattingName(obj.fio)}, ` : ''
     let school = obj.school ? `${obj.school}, ` : ''
     let fioTeacher = obj.fio_teacher ? `пед. ${obj.fio_teacher}, ` : ''
     return `${authorName}${material}${age}${fio}${school}${fioTeacher} `
-
 
 }
