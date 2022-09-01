82014dcb53749b547139f349c41fc17034a91dc1
nezaicev <nezaicev@yandex.ru>
2022-09-01 13:41:26 +0300

    add scroll to CreativTack

--- frontend/js/src/components/Gallary/CreativeTack.js
+++ frontend/js/src/components/Gallary/CreativeTack.js
@@ -6,32 +6,49 @@ import AssignmentIcon from '@mui/icons-material/Assignment';
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import {Paper} from "@mui/material";
 
 export default function CreativeTack(props) {
     if (props.data.length !== 0) {
         return (
             <Card sx={{
-                    margin:'25px',
-                    padding: '45px',
-                    boxShadow: 0,
-                    border: 2,
-                    borderColor: '#dad2d2'
-                }}>
+                margin: '25px',
+                padding: ['5px','10px'],
+                boxShadow: 0,
+                border: 2,
+                borderColor: '#dad2d2',
+
+            }}
+
+            >
 
 
-                    <Tooltip title="Творческое задание" sx={{marginLeft:'-40px', marginTop:'-40px'}}>
-                        <IconButton>
-                            <AssignmentIcon sx={{
-                                fontSize: '2rem',
-                                color: 'rgb(128,110,110)',
-                                padding: '2px'
-                            }}/>
-                        </IconButton>
-                    </Tooltip>
-                 <Box component='div'
-                      dangerouslySetInnerHTML={{__html: props.data}}>
+                <Tooltip title="Творческое задание">
+                    <IconButton>
+                        <AssignmentIcon sx={{
+                            fontSize: '2rem',
+                            color: 'rgb(128,110,110)',
+                            padding: '2px'
+                        }}/>
+                    </IconButton>
+                </Tooltip>
 
-                </Box>
+                <Paper
+                     // overflow={'auto'}
+                     // maxHeight={600}
+                     dangerouslySetInnerHTML={{__html: props.data}}
+                     scroll={'body'}
+                     sx={{
+                         boxShadow: 0,
+                         maxHeight:600,
+                         overflow:'auto',
+                         padding:['5px','25px'],
+                     }}
+
+                >
+
+
+            </Paper>
             </Card>
         )
     }
