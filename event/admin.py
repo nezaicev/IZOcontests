c03628d15c6e7079fae00ce8c1989b8483941ee9
nezaicev <nezaicev@yandex.ru>
2022-10-31 13:47:50 +0300

    add send letter for participants event

--- event/admin.py
+++ event/admin.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.template.response import TemplateResponse
 from django_changelist_toolbar_admin.admin import DjangoChangelistToolbarAdmin
 from django_simple_export_admin.admin import DjangoSimpleExportAdmin
 
+from contests import tasks
 from event.models import ParticipantEvent, Event
+from mailing.forms import SelectLetterForm
+from mailing.models import Email
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -16,6 +20,7 @@ class ParticipantEventAdmin(DjangoSimpleExportAdmin, admin.ModelAdmin):
                     'certificate']
     list_filter=['event',]
     exclude = ['reg_number',]
+    actions = ['send_selected_letter',]
 
     django_simple_export_admin_exports = {
         "filtered-participant": {
@@ -38,6 +43,34 @@ class ParticipantEventAdmin(DjangoSimpleExportAdmin, admin.ModelAdmin):
                 "event.export_participants"],
         }
     }
+    def send_selected_letter(self, request, queryset):
+
+        if 'apply' in request.POST:
+            form = SelectLetterForm(request.POST)
+            if form.is_valid():
+                letter = Email.objects.get(id=form.cleaned_data['letters'])
+                recipients = list(
+                    queryset.values_list('participant_id__email', flat=True))
+                if recipients:
+                    tasks.send_mail_for_subscribers.delay(recipients,
+                                                          letter.theme,
+                                                          letter.content)
+                    messages.add_message(request, messages.INFO,
+                                         'Письмо {}_{}, отправлено - {} '.format(
+                                             letter.date,
+                                             letter.theme,
+                                             len(recipients)))
+            return None
+
+        form = SelectLetterForm(
+            initial={
+                '_selected_action': queryset.values_list('id',
+                                                         flat=True), })
+        return TemplateResponse(request, "admin/select_letter.html",
+                                {'items': queryset, 'form': form})
+
+    send_selected_letter.short_description = 'Отправить выбранное письмо'
+    # send_selected_letter.allowed_permissions = ('execution_all_actions',)
 
     def get_queryset(self, request):
         if request.user.is_superuser or request.user.groups.filter(
