9a691ee8482c6f6712f2d347461013604ab93ead
nezaicev <nezaicev@yandex.ru>
2023-05-19 08:57:35 +0300

    fix bug layout exposition card

--- frontend/js/src/components/Exposition/CardExposition.js
+++ frontend/js/src/components/Exposition/CardExposition.js
@@ -1,5 +1,5 @@
 import Card from "@mui/material/Card";
-import {CardMedia, Chip, ImageListItemBar} from "@mui/material";
+import {CardMedia, Chip, ImageListItemBar, Paper} from "@mui/material";
 import Box from "@mui/material/Box";
 
 import React from "react";
@@ -10,12 +10,12 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 import IconButton from "@mui/material/IconButton";
 import {host} from "../utils/consts";
 import {Link} from "react-router-dom"
-
-
+import Tooltip from "@mui/material/Tooltip";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 
 
 function CardExposition(props) {
-    const urlExposition=`${host}/exposition/${props.data.id}/`
+    // const urlExposition = `${host}/exposition/${props.data.id}/`
     let optionsDate = {
         year: 'numeric',
         month: 'numeric',
@@ -23,11 +23,10 @@ function CardExposition(props) {
 
     };
     return (
-        <ImageListItem
-            sx={{marginTop: '15px', margin:'10'}}
 
-        >
-            <Card sx={{border: 7, borderColor: '#fff', boxShadow: 0}}>
+        <ImageListItem >
+
+            <Paper>
                 <Link to={`/exposition/${props.data.id}`}>
                     <img
                         src={props.data.poster['thumb']}
@@ -35,14 +34,16 @@ function CardExposition(props) {
                         loading="lazy"
                     />
                 </Link>
-                { props.data.poster['thumb'] ? <ImageListItemBar
+
+
+                {props.data.poster['thumb'] ? <ImageListItemBar
                     sx={{
                         backgroundColor: "rgba(138, 119, 119, 0.89)"
                     }}
                     title={
-                        props.data['virtual']? 'Виртуальная выставка':
-                        getFormattedDate(props.data['start_date'], optionsDate) + ' - ' +
-                    getFormattedDate(props.data['end_date'], optionsDate)
+                        props.data['virtual'] ? 'Виртуальная выставка' :
+                            getFormattedDate(props.data['start_date'], optionsDate) + ' - ' +
+                            getFormattedDate(props.data['end_date'], optionsDate)
                     }
                     actionIcon={
 
@@ -53,11 +54,49 @@ function CardExposition(props) {
                             }}/>
                         </IconButton>
 
-                    }/>:''}
+                    }/> : ''}
 
-            </Card>
+
+            </Paper>
 
         </ImageListItem>
+
+
+        // <ImageListItem
+        //     sx={{marginTop: '15px', margin:'10'}}
+        //
+        // >
+        //     <Card sx={{border: 7, borderColor: '#fff', boxShadow: 0}}>
+        //         <Link to={`/exposition/${props.data.id}`}>
+        //             <img
+        //                 src={props.data.poster['thumb']}
+        //                 alt={props.data.title}
+        //                 loading="lazy"
+        //             />
+        //         </Link>
+        //         { props.data.poster['thumb'] ? <ImageListItemBar
+        //             sx={{
+        //                 backgroundColor: "rgba(138, 119, 119, 0.89)"
+        //             }}
+        //             title={
+        //                 props.data['virtual']? 'Виртуальная выставка':
+        //                 getFormattedDate(props.data['start_date'], optionsDate) + ' - ' +
+        //             getFormattedDate(props.data['end_date'], optionsDate)
+        //             }
+        //             actionIcon={
+        //
+        //                 <IconButton>
+        //                     <EventNoteIcon sx={{
+        //                         color: "#fff",
+        //                         p: '5px'
+        //                     }}/>
+        //                 </IconButton>
+        //
+        //             }/>:''}
+        //
+        //     </Card>
+        //
+        // </ImageListItem>
 
     )
 }
