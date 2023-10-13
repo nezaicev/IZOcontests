1b5e3034db8745f570ceeac81953b59aba90bdfc
nezaicev <nezaicev@yandex.ru>
2023-10-13 14:27:13 +0300

    update hideDistrict

--- static/admin/js/hideDistrictField.js
+++ static/admin/js/hideDistrictField.js
@@ -1,12 +1,17 @@
-function hideDistrict() {
-    let region = $("#id_region")
-    let district = $(".field-district")
-    region.change(function () {
-        if (region.find(":selected").text() === 'г. Москва') {
+const action = (region, district)=>{
+    if (region.find(":selected").text() === 'г. Москва') {
             district.show()
         } else {
             district.hide()
         }
+}
+
+function hideDistrict() {
+    let region = $("#id_region")
+    let district = $(".field-district")
+    action(region, district)
+    region.change(function () {
+     action(region, district)
     })
 }
 
