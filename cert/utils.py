5719805758a9a9ad16c105cab69ecd2e15ae566b
nezaicev <nezaicev@yandex.ru>
2024-05-13 12:35:48 +0300

    update city for cert

--- cert/utils.py
+++ cert/utils.py
@@ -119,7 +119,7 @@ def generate_cert(reg_number, blank_cert, form_values):
                            blank_cert.city_text.anchor
                            )
 
-    prev_field_position=blank_cert.city_text.offset+city[3] if blank_cert.city_text and city else prev_field_position=blank_cert.position_text.offset+position[3]
+    prev_field_position=blank_cert.city_text.offset+city[3] if blank_cert.city_text and city else blank_cert.school_text.offset+school[3]
     if form_values.get('teacher') and blank_cert.teacher_text and form_values.get('owner')=='Участник':
         blank_cert.teacher_text.position[1] = prev_field_position
         label_teacher = insert_text(font_default,
